04c671b629004b811dd72fedd0ac10a42ac2f63b
Vishesh Singhal <singhalvishesh04@gmail.com>
2025-07-20 02:13:59 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter a number";
+    cin>>a;
+    if(a%2!=0)
+    cout<<"It is a odd number";
+    else
+    cout<<"It is an even number";
+
+    return 0;
+}
---
+++ operators.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 5;
+
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a / b = " << a / b << endl;
+    cout << "a % b = " << a % b << endl;
+
+    cout << "Is a > b? " << (a > b) << endl; // 1 for true,0 for false
+    cout << "Is a == b? " << (a == b) << endl; //similar
+
+    return 0;
+}
