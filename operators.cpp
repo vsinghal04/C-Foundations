fd6ffd406c8f24942e919c2db004078f2391796f
Vishesh Singhal <singhalvishesh04@gmail.com>
2025-07-20 02:15:49 +0530

    Update operators.cpp

--- operators.cpp
+++ operators.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
     int a = 10, b = 5;
 
-    cout << "a + b = " << a + b << endl;
-    cout << "a - b = " << a - b << endl;
-    cout << "a * b = " << a * b << endl;
-    cout << "a / b = " << a / b << endl;
+    cout << "a + b = " << a + b << endl; //addition
+    cout << "a - b = " << a - b << endl; //sub
+    cout << "a * b = " << a * b << endl; //multiply
+    cout << "a / b = " << a / b << endl;  //mod
     cout << "a % b = " << a % b << endl;
 
     cout << "Is a > b? " << (a > b) << endl; // 1 for true,0 for false
