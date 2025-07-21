54b22562005c8c97ce3299d64fddedc1b8497b9b
Vishesh Singhal <singhalvishesh04@gmail.com>
2025-07-21 10:39:03 +0530

    Add files via upload

---
+++ trianglearea.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,area;
+    cout<<"Enter lenght and breadth";
+    cin>>a>>b;
+    area=0.5*a*b;
+    cout<<"area is "<<area;
+
+
+    return 0;
+}
